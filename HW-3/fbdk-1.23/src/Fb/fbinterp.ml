468fa4a2b5640908707e85af09701aeca7ebe274
Srinivas <srinivas.suresh.kumar@gmail.com>
2017-02-28 00:31:45 -0500

    Implemented bools, yayus

--- HW-3/fbdk-1.23/src/Fb/fbinterp.ml
+++ HW-3/fbdk-1.23/src/Fb/fbinterp.ml
@@ -1,6 +1,21 @@
 open Fbast;;
 
+exception TypeMismatch;;
+
 (*
  * Replace this with your interpreter code.
  *)
-let rec eval e = e
+let rec eval e =
+    match e with
+    | And (e1, e2) ->
+        (match (eval e1, eval e2) with
+             (Bool x, Bool y) -> Bool (x && y)
+            | _ -> raise TypeMismatch
+        )
+    | Or (e1, e2) ->
+        (match (eval e1, eval e2) with
+             (Bool x, Bool y) -> Bool (x || y)
+            | _ -> raise TypeMismatch
+        )
+    | _ -> e
+;;        
